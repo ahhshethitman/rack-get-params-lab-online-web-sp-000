22474795482fc21c6f3f9734baa552b699757a97
Hunter Sides <hunter.s.1337@gmail.com>
2020-09-17 16:22:22 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -21,6 +21,16 @@ class Application
     else 
       @@cart.each do |item| 
         resp.write "#{item}\n"
+      end 
+    end 
+  elsif req.path.mathc(/add/)
+    search_term = req.params["item"]
+    if @@items.include?(search_term)
+      @@cart << search_term 
+      resp.write "added #{search_term}"
+    else 
+      resp.write "We don't have that item"
+    end 
     else
       resp.write "Path Not Found"
     end
